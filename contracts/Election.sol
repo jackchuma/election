11f9cd6462f55d3f2593e91f09e6547e94e5d14c
Jack Chuma <jackchuma21@gmail.com>
2022-03-17 18:18:56 -0400

    test added: should store names of candidates after deployment

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Election {
-    string public _candA;
-    string public _candB;
+contract Election is Ownable {
+    using Counters for Counters.Counter;
+
+    string public candA;
+    string public candB;
+
+    Counters.Counter public aTotal;
+    Counters.Counter public bTotal;
 
     constructor(string memory _candidateA, string memory _candidateB) {
-        _candA = _candidateA;
-        _candB = _candidateB;
+        candA = _candidateA;
+        candB = _candidateB;
     }
 }
--- test/Election.test.js
+++ test/Election.test.js
@@ -1,10 +1,22 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
 describe("Election", function () {
   before(async function() {
-    const Election = await ethers.getContractFactory("Election");
-    this.election = await Election.deploy("Elon Musk", "Jeff Bezos");
-    await this.election.deployed();
+    this.signers = ethers.getSigners();
+    this.Election = await ethers.getContractFactory("Election");
+  });
+
+  context("Deployment", async function() {
+    this.beforeEach(async function() {
+      this.election = await this.Election.deploy("Elon Musk", "Jeff Bezos");
+      await this.election.deployed();
+    });
+
+    it ("Should store names of candidates after deployment", async function() {
+      expect(await this.election.candA()).to.equal("Elon Musk");
+      expect(await this.election.candB()).to.equal("Jeff Bezos");
+    });
   });
 });
