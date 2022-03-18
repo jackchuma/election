48fb617226f129ab80f80a73fdba9a88ca5c3ebb
Jack Chuma <jackchuma21@gmail.com>
2022-03-18 18:10:23 -0400

    test added: expectedVotes must be set to start new election

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -126,6 +126,7 @@ contract Election is Ownable {
     function newElection() external view onlyOwner {
         require(!_equalStrings(candA, ""), "set candA");
         require(!_equalStrings(candB, ""), "set candB");
+        require(expectedVotes > 0, "set expectedVotes");
     }
 
     function _equalStrings(string memory _strA, string memory _strB) private pure returns (bool) {
